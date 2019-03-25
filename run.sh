135f758dc7f78869c65b25d89d888faa2a34ce74
iradu <ionut.radu@tremend.com>
2019-03-25 17:58:20 +0200

    - first commit

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ BODY=`git log -1 --pretty=format:"%b"`
 BRANCH=$WERCKER_GIT_BRANCH
 # convert to uppercase
 BRANCH="${BRANCH^^}"
+echo $BODY
 BODY="${BODY^^}"
 if [[ ! $BRANCH =~ ^ESDT-[0-9]+$ ]]; then
         echo "Branch $BRANCH not valid"
@@ -18,7 +19,7 @@ NR_ESDT=`echo $BODY| grep  -w -Eo "ESDT-[0-9]+" | wc -l `
 NR_ESDT2=`echo $BODY| grep  -w -Eo "ESDT" | wc -l`
 if [[ "$NR_ESDT" -eq "$NR_ESDT2" ]];
 then
-        echo -e "Found valid ESDT:\n$ESDT"
+        echo -e "Found valid ESDT in commit:\n$ESDT"
 else
         echo -e "Found non valid ESDT. The format should be ESDT-[0-9]+"
         echo $BODY
